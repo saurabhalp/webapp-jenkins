736102f122ccf59f0c9949206f88434be14f7855
Saurabh Kumar <saurabhsinghalp.com>
2025-06-26 05:42:32 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello from CI/CD pipeline!'));
+
+app.listen(port, () => console.log(`App running on port ${port}`));
