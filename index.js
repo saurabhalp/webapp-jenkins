760c5fb900138348a126ca3d0d027ff8c85e5ea8
Saurabh Kumar <saurabhsinghalp.com>
2025-07-01 08:02:45 +0000

    adding changes

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello from CI/CD pipeline.....!'));
+app.get('/', (req, res) => res.send('Hello from CI/CD pipeline....!'));
 
 app.listen(port, () => console.log(`App running on port ${port}`));
